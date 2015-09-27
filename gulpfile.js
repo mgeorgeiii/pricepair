d077a1e8017625370c64fc6de8cc0d6e54ba0d13
Monte George <monte.george@comcast.net>
2015-09-27 11:41:19 -0700

    compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee']
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/rclick.js',
 		'components/scripts/pixgrid.js',
 		'components/scripts/tagline.js',
 		'components/scripts/template.js',
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
 	gulp.src()
@@ -21,6 +24,18 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/developments/js'))
-	
+})
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+			})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/developments/css'))
+
 })
